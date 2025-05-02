0a2d4890836a2e00f9bcda7a94f07ca4b3775341
snarlingpluto <simon.halir@gmail.com>
2025-05-02 09:10:58 +0200

    room_change

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,7 +7,12 @@ public class TopDownMovement : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 movement;
-
+    public Camera camera1;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        camera1.transform.position = collision.transform.position + new Vector3(0,0,-10);
+        
+    }
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -19,6 +24,8 @@ public class TopDownMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal"); // A/D or Left/Right
         movement.y = Input.GetAxisRaw("Vertical");   // W/S or Up/Down
         movement = movement.normalized; // To prevent faster diagonal movement
+
+
     }
 
     void FixedUpdate()
