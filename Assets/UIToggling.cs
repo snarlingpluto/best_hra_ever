638d608ca93387184e4d929e6f5fa98242a66da3
Jublim <111000vevo@gmail.com>
2025-06-05 22:13:46 +0200

    Clean up, preps

---
+++ Assets/UIToggling.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using TMPro;
+
+public class UIToggling : MonoBehaviour
+{
+    public PlayerMovement playerMovement;
+    public GameObject mainMenu;
+    public bool mainMenuOpen;
+    public GameObject gameUI;
+    public bool gameUIOpen;
+    public GameObject lossMenu;
+    public bool lossMenuOpen;
+
+    public GameObject startGameButton;
+    public GameObject playAgainButton;
+    public TMP_Text scoreMeter;
+
+    void Start()
+    {
+        playerMovement = FindAnyObjectByType<PlayerMovement>();
+        mainMenuOpen = true;
+        /* mainMenuToggle(); */
+        gameUIOpen = false;
+       /*  gameUI.SetActive = false; */
+        lossMenuOpen = false;
+        /* lossMenu.SetActive = false; */
+
+    }
+
+    void Update()
+    {
+        if (mainMenuOpen)
+        {
+
+        }
+    }
+}
