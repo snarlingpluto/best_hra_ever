8093ae19c4d55b9ee4d53c7e50036ad50bbef3b8
Jublim <111000vevo@gmail.com>
2025-06-11 22:14:12 +0200

    Fixed UI on maximized

---
+++ Assets/AspectRatioSet.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+public class AspectRatio : MonoBehaviour
+{
+    public Camera mainCameraRatio;
+    void Start()
+    {
+/*         mainCameraRatio.AspectRatio(16 / 9); */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
